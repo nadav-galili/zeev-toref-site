b4e659b60515982ce450cd50de1498b2472f31f2
nadav-galili <67638018+nadav-galili@users.noreply.github.com>
2020-10-12 11:15:58 +0300

    add ons on index.html
    added animation,
    border on cards;
    fixed footer waze and whatsapp

---
+++ main.js
@@ -0,0 +1,31 @@
+// const text = document.querySelector('.fancy');
+// const srtText = text.textContent;
+// const splitText = srtText.split("");
+// text.textContent = "";
+// // console.log(splitText)
+
+// for (let i = 0; i < splitText.length; i++) {
+//     text.innerHTML += '<span>' + splitText[i] + '</span>';
+//     console.log(1)
+// }
+
+// let char = 0;
+// let timer = setInterval(onTick, 50);
+// // console.log(2)
+
+// function onTick() {
+//     const span = text.querySelectorAll('span')[char];
+//     span.classList.add('fadeIt');
+//     char++;
+//     if (char === splitText.length) {
+//         complete();
+        
+//         return;
+//     } 
+// }
+
+// function complete() {
+//     clearInterval(timer);
+//     timer = null;
+//     console.log(5)
+// }
